65c629da03e1312f8caa366f7086a64411214417
javicia <javier.garcia.perez@hotmail.com>
2023-11-02 15:29:46 +0100

    creaciuón de repository y de creación de la clase exception

---
+++ src/main/java/com/sistema/blog/exceptions/ResourceNoteFoundException.java
@@ -0,0 +1,79 @@
+package com.sistema.blog.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNoteFoundException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2241845383667580104L;
+
+	private String nombreRecurso;
+	private String nombreCampo;
+	private long valorCampo;
+	
+	
+	
+	
+	
+	public ResourceNoteFoundException(String nombreRecurso, String nombreCampo, long valorCampo) {
+		super(String.format("%s No encontrado con : %s : '%s'", nombreRecurso, nombreCampo, valorCampo ));
+		this.nombreRecurso = nombreRecurso;
+		this.nombreCampo = nombreCampo;
+		this.valorCampo = valorCampo;
+	}
+
+
+
+
+
+	public String getNombreRecurso() {
+		return nombreRecurso;
+	}
+
+
+
+
+
+	public void setNombreRecurso(String nombreRecurso) {
+		this.nombreRecurso = nombreRecurso;
+	}
+
+
+
+
+
+	public String getNombreCampo() {
+		return nombreCampo;
+	}
+
+
+
+
+
+	public void setNombreCampo(String nombreCampo) {
+		this.nombreCampo = nombreCampo;
+	}
+
+
+
+
+
+	public long getValorCampo() {
+		return valorCampo;
+	}
+
+
+
+
+
+	public void setValorCampo(long valorCampo) {
+		this.valorCampo = valorCampo;
+	}
+	
+	
+	
+}
