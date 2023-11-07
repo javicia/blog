429fa50b6d3995b7a5ce49d7c5e2b54292c51367
Javier <javigpdeveloper@gmail.com>
2023-11-07 01:53:41 +0100

    Token

---
+++ src/main/java/com/sistema/blog/security/JwtTokenProvider.java
@@ -0,0 +1,31 @@
+package com.sistema.blog.security;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtTokenProvider {
+
+	@Value("${app.jwt-secret}")
+	private String jwtSecret;
+	
+	@Value("${app.jwt-expiration-milliseconds}")
+	private int jwtExpirationInMs;
+	
+	public String generarToken(Authentication authentication) {
+		String username = authentication.getName();
+		Date fechaActual = new Date();
+		Date fechaExpiracion = new Date(fechaActual.getTime() + jwtExpirationInMs);
+		
+		String token = Jwts.builder().setSubject(username).setIssuedAt(new Date()).setExpiration(fechaExpiracion)
+				.signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
+		return token;
+		
+	}
+}
